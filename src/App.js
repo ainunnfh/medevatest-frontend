2ae55873b952f3a37a1ec21042a825c1e0f61ff3
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-27 12:55:19 +0700

    🆙app.js: usage some components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  Form,
+  Table,
+  Dropdown,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import EmployeeData from "./components/EmployeeData";
+import EmployeeForm from "./components/EmployeeForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Sidebar>
+        <Container fluid>
+          <Row>
+            <Col md={5} sm={12} className="mb-3">
+              <EmployeeData />
+            </Col>
+
+            <Col md={7} sm={12} className="mb-3">
+              <EmployeeForm />
+            </Col>
+          </Row>
+        </Container>
+      </Sidebar>
+    </>
   );
 }
 
