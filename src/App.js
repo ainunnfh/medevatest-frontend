1a63dbd4f6997e1b9933ec569ed42ec7766da50b
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-03-28 03:59:43 +0700

    🆙app: create props n state for set selected employee id

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import axios from "axios";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "./components/Sidebar";
@@ -9,6 +8,7 @@ import EmployeeForm from "./components/EmployeeForm";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [selectedEmployeeId, setSelectedEmployeeId] = useState(null);
   return (
     <>
       <Navbar />
@@ -16,11 +16,22 @@ function App() {
         <Container fluid>
           <Row>
             <Col md={5} sm={12} className="mb-3">
-              <EmployeeData onAddEmployee={() => setShowForm(true)} />
+              <EmployeeData
+                onAddEmployee={() => setShowForm(true)}
+                onEditClick={(id) => {
+                  setSelectedEmployeeId(id);
+                  setShowForm(true);
+                }}
+              />
             </Col>
 
             <Col md={7} sm={12} className="mb-3">
-              {showForm && <EmployeeForm />}
+              {showForm && (
+                <EmployeeForm
+                  employeeId={selectedEmployeeId}
+                  onClose={() => setShowForm(false)}
+                />
+              )}
             </Col>
           </Row>
         </Container>
